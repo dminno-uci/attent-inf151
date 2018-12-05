958ae19093a2719ee1d4029ca0648cf960443d64
Ricky <minnojr@Rickys-MacBook-Pro.local>
2018-12-05 15:52:02 -0800

    hopefully final commit before presentation

--- src/app/components/view/view.component.ts
+++ src/app/components/view/view.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ShareService } from '../../service/share.service';
 import {map} from 'rxjs/operators';
 import {Observable} from 'rxjs';
+import {utf8Encode} from '@angular/compiler/src/util';
+import { Base64 } from 'js-base64';
 
 
 @Component({
@@ -11,14 +13,24 @@ import {Observable} from 'rxjs';
 })
 export class ViewComponent implements OnInit {
   public images: Observable<any[]>;
+  // public realImages: Image[];
 
   constructor(private shareservice: ShareService) { }
 
   ngOnInit() {
-    this.images = this.getShares('/dataImages').pipe(map((array) => array.reverse()));
+    this.images = this.getImages('/dataImages').pipe(map((array) => {
+      return array.map(d => {
+        if (d.wordcloud) {
+          return 'data:image/png;base64,' + d.wordcloud;
+        } else if (d.doughnutplot) {
+          return 'data:image/png;base64,' + d.doughnutplot;
+        }
+      });
+    }));
   }
-  getShares(path) {
-    return this.shareservice.getImages(path, 1);
+  getImages(path) {
+    return this.shareservice.getImages(path, 2);
   }
+
 
 }
